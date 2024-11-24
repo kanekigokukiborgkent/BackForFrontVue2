6769a9c88cc72ef48517209654889e643d4d9d7f
kanekigokukiborgkent <msavciuc00@mail.ru>
2024-11-24 15:54:10 +0200

    Add project files.

---
+++ Controllers/CallBackFrontController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackForFrontVue2.Controllers
+{
+    [ApiController]
+    [Route("CallBackFront")]
+    public class CallBackFrontController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetWeather()
+        {
+            var weather = new { Temp = 20, Condition = "Sunny" };
+            return Ok(weather);
+        }
+
+        [HttpPost]
+        public IActionResult PostWeather([FromBody] WeatherRequest request)
+        {
+            return Ok(new { Message = "Weather updated", Data = request });
+        }
+    }
+
+    public class WeatherRequest
+    {
+        public int Temp { get; set; }
+        public string Condition { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BackForFrontVue2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowVueApp", policy =>
+                    policy.WithOrigins("http://localhost:8080")  
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+            });
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddAuthorization();
+
+            var app = builder.Build();
+
+            app.UseCors("AllowVueApp");
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
